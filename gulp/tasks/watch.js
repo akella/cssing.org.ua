98b6cd009fd81abbbe1eccbec77d1ffa991666ed
akella <akella.a@gmail.com>
2017-01-08 17:55:08 +0200

    lets start

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,12 @@
+var gulp   = require('gulp');
+var config = require('../config');
+
+gulp.task('watch', 
+    ['copy:watch',
+    
+    'nunjucks:watch',
+    'sprite:svg:watch',
+    'svgo:watch',
+    'webpack:watch',
+    'sss:watch'
+]);
